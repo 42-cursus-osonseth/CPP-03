3f11c9a8221ef3c61c4b38df2479040c43f4a153
Osonseth <max.cesaria@gmail.com>
2024-12-23 16:38:37 +0100

    Ready

--- Ex01/ScavTrap.cpp
+++ Ex01/ScavTrap.cpp
@@ -12,12 +12,6 @@ ScavTrap::ScavTrap(std::string n) : ClapTrap (n, 100, 50, 20)
      std::cout
     << MAGENTA << "SCAVTRAP constructor is called" << RESET << std::endl;
 }
-void ScavTrap::guardGate() const
-{
-    std::cout
-    << BLUE_BOLD << getName() << RESET
-    << YELLOW_ITALIC << " is now in Gate keeper mode!" << RESET << std::endl;
-}
 ScavTrap::ScavTrap(const ScavTrap &other) : ClapTrap(other)
 {
     std::cout
@@ -41,4 +35,10 @@ void ScavTrap::attack(const std::string &target)
 {
     std::cout << RED << "The Scavtrap ";
     ClapTrap::attack(target);
+}
+void ScavTrap::guardGate() const
+{
+    std::cout
+    << BLUE_BOLD << getName() << RESET
+    << YELLOW_ITALIC << " is now in Gate keeper mode!" << RESET << std::endl;
 }
