fe00b0b5cd1cccebc6934b8579eddb5f203211ac
Osonseth <max.cesaria@gmail.com>
2024-12-27 13:12:25 +0100

    CPP 03 pret pour evaluation

---
+++ ex02/main.cpp
@@ -0,0 +1,32 @@
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
+
+int main()
+{
+    FragTrap a("bob");
+    FragTrap b = a;
+    FragTrap c("Max");
+    ScavTrap d("scav");
+    ClapTrap e ("clap");
+    a = c;
+
+    a.printInfos();
+    b.printInfos();
+
+    while ((a.getHealth() > 0 || b.getHealth() > 0) && (a.getEnergy() > 0 || b.getEnergy() > 0))
+    {
+        a.beRepaired(15);
+        b.beRepaired(15);
+        a.attack(b.getName());
+        b.takeDamage(a.getAttackDamage());
+        b.attack(a.getName());
+        a.takeDamage(b.getAttackDamage());
+
+        a.highFivesGuys();
+        b.highFivesGuys();
+    }
+    d.attack("qdgdgdd");
+    e.attack("dfsqgsdgg");
+
+    return 0;
+}
