13cc6937809da728bbf600e8463210aa523bb2c9
Osonseth <max.cesaria@gmail.com>
2024-12-27 12:54:12 +0100

    CPP 03 pret pour evaluation

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,45 @@
+#ifndef ClapTrap_H
+#define ClapTrap_H
+
+#include <string>
+#include <iostream>
+#include "Colors.hpp"
+
+class ClapTrap
+{
+private:
+    std::string Name;
+    unsigned int Health;
+    unsigned int Energy;
+    unsigned int Attack_damage;
+    static const unsigned int MAX_HEALTH = 100;
+
+public:
+    // Constructors / Destructor
+    ClapTrap();
+    ClapTrap(std::string n);
+    ClapTrap(const ClapTrap &other);
+    ~ClapTrap();
+
+    // Operator Overloading
+    ClapTrap &operator=(const ClapTrap &other);
+
+    // Getters / Setters
+    const std::string &getName() const;
+    unsigned int getEnergy() const;
+    unsigned int getHealth() const;
+    unsigned int getAttackDamage() const;
+    void setHealth(unsigned int damage);
+    void setEnergy();
+
+    // Member functions
+    void attack(const std::string &target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+    void printAttack(const std::string &target) const;
+    void printDamage(unsigned int amount) const;
+    void printRepaired(unsigned int amount) const;
+    void printInfos() const;
+};
+
+#endif
